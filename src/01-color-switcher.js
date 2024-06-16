cf1a30263a3b78296a7d7de620afd9042064a5c3
Juraj Marcinko <jurajmar@yahoo.ie>
2024-06-16 23:17:32 +0200

    update

--- src/01-color-switcher.js
+++ src/01-color-switcher.js
@@ -1,24 +1,32 @@
-// 01-color-switcher.js
-
+// Funkcja do generowania losowego koloru
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
-const startButton = document.getElementById('startButton');
-const stopButton = document.getElementById('stopButton');
-let intervalId;
+// Wybieranie obu przycisków i powiązanie ich z elementami HTML
+const buttonStart = document.querySelector('[data-start]');
+const buttonStop = document.querySelector('[data-stop]');
 
-startButton.addEventListener('click', () => {
-  intervalId = setInterval(() => {
+let timeoutId = null;
+
+// Funkcja do rozpoczęcia zmiany koloru tła co sekundę, 1000ms
+function startChangingBackgroundColor() {
+  buttonStart.disabled = true;
+  buttonStop.disabled = false;
+
+  // Przypisanie ID timeout do zmiennej `timeoutId`
+  timeoutId = setTimeout(function changeColor() {
     document.body.style.backgroundColor = getRandomHexColor();
+    timeoutId = setTimeout(changeColor, 1000);
   }, 1000);
+}
 
-  startButton.disabled = true;
-  stopButton.disabled = false;
-});
+// Funkcja do zatrzymania zmiany koloru tła
+function stopChangingBackgroundColor() {
+  clearTimeout(timeoutId);
 
-stopButton.addEventListener('click', () => {
-  clearInterval(intervalId);
-  startButton.disabled = false;
-  stopButton.disabled = true;
-});
+  buttonStart.disabled = false;
+  buttonStop.disabled = true;
+}
+buttonStart.addEventListener('click', startChangingBackgroundColor);
+buttonStop.addEventListener('click', stopChangingBackgroundColor);
