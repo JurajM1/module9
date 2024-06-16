4b0558305456462cd8b6d1bedee8b5f134a65258
Juraj Marcinko <jurajmar@yahoo.ie>
2024-06-16 21:47:47 +0200

    update

---
+++ src/01-color-switcher.js
@@ -0,0 +1,24 @@
+// 01-color-switcher.js
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+const startButton = document.getElementById('startButton');
+const stopButton = document.getElementById('stopButton');
+let intervalId;
+
+startButton.addEventListener('click', () => {
+  intervalId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+
+  startButton.disabled = true;
+  stopButton.disabled = false;
+});
+
+stopButton.addEventListener('click', () => {
+  clearInterval(intervalId);
+  startButton.disabled = false;
+  stopButton.disabled = true;
+});
